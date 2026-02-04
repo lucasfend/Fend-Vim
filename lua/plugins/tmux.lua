f4ef5dfae1977d8e25729fbe335b857d547b8bf1
lucasfend <lucascomercial199@outlook.com>
2026-02-03 21:29:27 -0300

    fix tmux bugs

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,5 +1,6 @@
+local inside_tmux = vim.env.TMUX ~= nil
+
 return {
-    -- keybinds
     {
         "christoomey/vim-tmux-navigator",
         cmd = { "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp", "TmuxNavigateRight", "TmuxNavigatePrevious" },
@@ -13,11 +14,26 @@ return {
     },
 
     {
+        "vimpostor/vim-tpipeline",
+        dependencies = { "nvim-lualine/lualine.nvim" },
+        enabled = inside_tmux,
+        config = function()
+            vim.opt.cmdheight = 0
+            vim.opt.laststatus = 0
+        end,
+    },
+
+    {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         init = function()
             vim.opt.cmdheight = 0
-            vim.opt.laststatus = 3
+
+            if inside_tmux then
+                vim.opt.laststatus = 0
+            else
+                vim.opt.laststatus = 3
+            end
         end,
         opts = function(_, opts)
             opts.options.globalstatus = true
@@ -25,8 +41,6 @@ return {
             opts.options.section_separators = { left = "", right = "" }
 
             local colors = {
-                bg = "#161616",
-                bubble = "#262626",
                 text = "#dde1e6",
                 dim = "#525252",
                 pink = "#ff7eb6",
@@ -40,22 +54,20 @@ return {
                 trans = "None",
             }
 
-            local function bubble_style(fg_color)
-                return { bg = colors.bubble, fg = fg_color, gui = "bold" }
-            end
-
-            local function has_diagnostic()
-                local d = vim.diagnostic.get(0)
-                return d and #d > 0
-            end
-
-            local function has_lsp()
-                local clients = vim.lsp.get_clients()
-                return next(clients) ~= nil
+            local function txt_style(fg_color)
+                return { bg = colors.trans, fg = fg_color, gui = "bold" }
             end
 
             local function has_branch()
                 return vim.b.gitsigns_status_dict ~= nil or vim.b.gitsigns_head ~= nil
+            end
+            local function has_diagnostic()
+                local d = vim.diagnostic.get(0)
+                return d and #d > 0
+            end
+            local function has_lsp()
+                local clients = vim.lsp.get_clients()
+                return next(clients) ~= nil
             end
 
             local function split(condition)
@@ -63,7 +75,7 @@ return {
                     function()
                         return "│"
                     end,
-                    color = { fg = colors.bg, bg = colors.bubble },
+                    color = { fg = colors.dim, bg = colors.trans },
                     padding = { left = 0, right = 0 },
                     cond = condition,
                 }
@@ -85,23 +97,18 @@ return {
                     fmt = function(str)
                         return str:sub(1, 1)
                     end,
-                    color = bubble_style(colors.pink),
-                    separator = { left = "", right = "" },
+                    color = txt_style(colors.pink),
                     padding = { left = 1, right = 1 },
                 },
-
                 split(),
-
                 {
                     "branch",
                     icon = "",
-                    color = bubble_style(colors.purple),
+                    color = txt_style(colors.purple),
                     padding = { left = 1, right = 1 },
                     cond = has_branch,
                 },
-
                 split(has_branch),
-
                 {
                     function()
                         local fname = vim.fn.expand("%:t")
@@ -116,38 +123,29 @@ return {
                                 icon = f_icon .. " "
                             end
                         end
-                        local modified = vim.bo.modified and " - has changes" or ""
+                        local modified = vim.bo.modified and " ●" or ""
                         local readonly = (vim.bo.readonly or not vim.bo.modifiable) and " " or ""
                         return icon .. fname .. modified .. readonly
                     end,
-                    color = bubble_style(colors.blue),
-                    separator = { right = "" },
+                    color = txt_style(colors.blue),
                     padding = { left = 1, right = 1 },
                 },
             }
 
             opts.sections.lualine_x = {
                 {
-                    function()
-                        return ""
-                    end,
-                    color = { fg = colors.bubble, bg = colors.bg },
-                    padding = { left = 0, right = 0 },
-                },
-                {
                     "diagnostics",
                     sources = { "nvim_diagnostic" },
                     symbols = { error = " ", warn = " ", info = " ", hint = " " },
                     diagnostics_color = {
-                        error = { fg = colors.red, bg = colors.bubble },
-                        warn = { fg = colors.pink, bg = colors.bubble },
-                        info = { fg = colors.cyan, bg = colors.bubble },
-                        hint = { fg = colors.text, bg = colors.bubble },
+                        error = { fg = colors.red, bg = colors.trans },
+                        warn = { fg = colors.pink, bg = colors.trans },
+                        info = { fg = colors.cyan, bg = colors.trans },
+                        hint = { fg = colors.text, bg = colors.trans },
                     },
                     padding = { left = 0, right = 1 },
                 },
                 split(has_diagnostic),
-
                 {
                     function()
                         local clients = vim.lsp.get_clients()
@@ -161,22 +159,20 @@ return {
                             end
                         end
                         if #client_names > 0 then
-                            return "  LSP ~ " .. table.concat(client_names, ", ")
+                            return "  " .. table.concat(client_names, ", ")
                         end
                         return ""
                     end,
-                    color = bubble_style(colors.teal),
+                    color = txt_style(colors.teal),
                     padding = { left = 1, right = 1 },
                 },
                 split(has_lsp),
-
                 {
                     "filesize",
-                    color = bubble_style(colors.cyan),
+                    color = txt_style(colors.cyan),
                     padding = { left = 1, right = 1 },
                 },
                 split(),
-
                 {
                     function()
                         local line = vim.fn.line(".")
@@ -185,8 +181,7 @@ return {
                         local percent = math.floor((line / total) * 100)
                         return string.format("%d%%%% %d:%d", percent, line, col)
                     end,
-                    color = bubble_style(colors.lblue),
-                    separator = { right = "" },
+                    color = txt_style(colors.lblue),
                     padding = { left = 1, right = 1 },
                 },
             }
